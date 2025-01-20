bd10a2bfef37ba0dd55dc8016686796cce2ee9b9
Md Toufiquzzaman <150599497+runtimeninja@users.noreply.github.com>
2025-01-21 01:51:41 +0600

    implemented efficient climbingLeaderboard function with dense ranking logic

---
+++ climbing-the-leaderboard/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'climbingLeaderboard' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY ranked
+ *  2. INTEGER_ARRAY player
+ */
+
+func climbingLeaderboard(ranked []int32, player []int32) []int32 {
+	// Write your code here
+	// removed duplicates and created a dense ranking
+	uniqueRanks := []int32{}
+	for i, score := range ranked {
+		if i == 0 || score != ranked[i-1] {
+			uniqueRanks = append(uniqueRanks, score)
+		}
+	}
+
+	results := make([]int32, len(player))
+	n := len(uniqueRanks)
+	rankIndex := n - 1
+
+	for i, score := range player {
+		// traversed the unique ranks to find the player's rank
+		for rankIndex >= 0 && score >= uniqueRanks[rankIndex] {
+			rankIndex--
+		}
+		results[i] = int32(rankIndex + 2) // rank is index + 2 as rankIndex is 0-based
+	}
+
+	return results
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	rankedCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	rankedTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ranked []int32
+
+	for i := 0; i < int(rankedCount); i++ {
+		rankedItemTemp, err := strconv.ParseInt(rankedTemp[i], 10, 64)
+		checkError(err)
+		rankedItem := int32(rankedItemTemp)
+		ranked = append(ranked, rankedItem)
+	}
+
+	playerCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	playerTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var player []int32
+
+	for i := 0; i < int(playerCount); i++ {
+		playerItemTemp, err := strconv.ParseInt(playerTemp[i], 10, 64)
+		checkError(err)
+		playerItem := int32(playerItemTemp)
+		player = append(player, playerItem)
+	}
+
+	result := climbingLeaderboard(ranked, player)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
